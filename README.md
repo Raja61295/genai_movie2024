874f53a9686cd47df6a45bf33571f16f9344cf99
Raja61295 <rajamuthaiah61295@gmail.com>
2024-12-12 21:41:49 +0530

    Final-Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # genai_movie2024
 This repository  focuses on movie recommender systems
+### Steps
+* Clone the repository using 'git clone' command used ''cd<repo name>'' to get inside the repository folder
+## Installation
+step 1: We need to create the virtual environment by the name of ''.venv''. This will be created using following cmd''python -m venv .venv''
+step 2: Activate the virtual env by using cmd:''source .venv/scripts/activate''
+step 3: Install all the dependencies ''pip install -r requirements.txt''
---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from langchain import PromptTemplate, LLMChain
+from dotenv import load_dotenv
+load_dotenv()
+import google.generativeai as genai
+import os
+#setup the api
+genai.configure(api_key = os.getenv('GOOGLE-API-KEY'))
+from langchain_google_genai import ChatGoogleGenerativeAI
+import warnings 
+warnings.filterwarnings("ignore")
+# Design the Page st.title("Movie Recommendation System using Gemini-Pro") 
+user_input = st.text_input("Enter movie title, genre or keywords (e.g. Sci-FI Movie): ")
+# Prompt Template 
+template = PromptTemplate(input_variables = ['user_input'], template = '''Based on Preferences, here are the recommendations for {user_input}:\n''')
+# Initialise the Model
+llm = ChatGoogleGenerativeAI(model = "gemini-pro")
+if user_input: 
+    prompt = template.format(user_input=user_input) 
+    recommendations = llm.predict(text = prompt) 
+    st.write(f'''Recommendations for You: \n{recommendations}''') 
+else: 
+    st.write("Please enter the Movie Preference")
