2c784e0bd8b6abec599dc2d2fd9cea03dcf35632
Raja61295 <rajamuthaiah61295@gmail.com>
2024-12-12 23:47:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ user_input = st.text_input("Enter movie title, genre or keywords (e.g. Sci-FI Mo
 # Prompt Template 
 template = PromptTemplate(input_variables = ['user_input'], template = '''Based on Preferences, here are the recommendations for {user_input}:\n''')
 # Initialise the Model
-<<<<<<< HEAD
-llm = ChatGoogleGenerativeAI(model = "gemini-pro",api_key=os.getenv('GOOGLE-API-KEY'))
-=======
 llm = ChatGoogleGenerativeAI(model = "gemini-pro",api_key = os.getenv('GOOGLE-API-KEY'))
->>>>>>> 2eeb0d165d6d2151d185216b0c52af4b22ad15dd
 if user_input: 
     prompt = template.format(user_input=user_input) 
     recommendations = llm.predict(text = prompt) 
